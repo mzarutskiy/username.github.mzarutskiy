cbf05ddcedf4740a2b551a637d2cfe92c8be3e09
mzarutskiy <147590461+mzarutskiy@users.noreply.github.com>
2023-10-18 14:37:58 +0300

    Add files via upload

--- Names_Changing.js
+++ Names_Changing.js
@@ -1,15 +1,15 @@
 const node_for_click = document.getElementById("for_click")
 
 function find_edit() {
-    const p0 = document.getElementsByTagName('p')[0]
+    const p0 = document.getElementsByTagName('p0')[0]
     console.log(p0.innerText)
     p0.innerText = 'Zarutskiy'
 
-    const p1 = document.getElementsByTagName('p')[1]
+    const p1 = document.getElementsByTagName('p1')[0]
     console.log(p1.innerText)
     p1.innerText = 'Mikhail'
 
-    const p2 = document.getElementsByTagName('p')[2]
+    const p2 = document.getElementsByTagName('p2')[0]
     console.log(p2.innerText)
     p2.innerText = 'Alekseevich'
 
